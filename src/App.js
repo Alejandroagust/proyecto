286cf7457422dd1be7ea04c80a77dd6789e320b4
agustin <pinky.program@gmail.com>
2024-08-26 17:52:58 -0300

    nuevo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Navbar from'./components/NavBar.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
